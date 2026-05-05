446c6939a9238ccd3c23364fdd94a22c18e0f08a
Sagarbompada <35405162+Sagarbompada@users.noreply.github.com>
2026-05-04 21:00:51 -0400

    Upgrade AWS provider version to ~> 6.0

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.55.0"
+      version = "~> 6.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
-  # Configuration options
   region = "us-east-1"
 }
